d5be7b396870e9059a9c267d0fbc2e936edd9227
jessefu <jessefu68@gmail.com>
2026-09-18 14:13:46 +0800

    fix: update csp for r2 domain

--- strapi-backend/config/middlewares.ts
+++ strapi-backend/config/middlewares.ts
@@ -11,6 +11,21 @@ const config: Core.Config.Middlewares = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          // 在下面这行里加入你的 R2 域名
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'pub-49bd4c71ca7f4c9dba57d9211d7b8e81.r2.dev'],
+          'media-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'pub-49bd4c71ca7f4c9dba57d9211d7b8e81.r2.dev'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
 
 export default config;
