5a6347886288298a6bf091a101c1203e6eee4884
jessefu <jessefu68@gmail.com>
2026-09-23 17:11:37 +0800

    add domain

--- strapi-backend/config/middlewares.ts
+++ strapi-backend/config/middlewares.ts
@@ -3,14 +3,7 @@ import type { Core } from '@strapi/strapi';
 const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  // 1. 注意：这里删除了原来简写的 'strapi::security'，保留下面带配置的版本
   {
     name: 'strapi::security',
     config: {
@@ -18,14 +11,34 @@ const config: Core.Config.Middlewares = [
         useDefaults: true,
         directives: {
           'connect-src': ["'self'", 'https:'],
-          // 在下面这行里加入你的 R2 域名
-          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'pub-49bd4c71ca7f4c9dba57d9211d7b8e81.r2.dev'],
-          'media-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'pub-49bd4c71ca7f4c9dba57d9211d7b8e81.r2.dev'],
+          'img-src': [
+            "'self'", 
+            'data:', 
+            'blob:', 
+            'market-assets.strapi.io', 
+            'resources.buywewant.com', // 👈 必须加上你绑定的 R2 自定义域名
+            '*.r2.dev'                  // 👈 允许所有 r2.dev 域名
+          ],
+          'media-src': [
+            "'self'", 
+            'data:', 
+            'blob:', 
+            'market-assets.strapi.io', 
+            'resources.buywewant.com', // 👈 必须加上你绑定的 R2 自定义域名
+            '*.r2.dev'                  // 👈 允许所有 r2.dev 域名
+          ],
           upgradeInsecureRequests: null,
         },
       },
     },
   },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
 ];
 
 export default config;
